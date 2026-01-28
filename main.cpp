830e7ee38d33f45415b9fee2ea200783b172ea25
KittayaApk <kittaya.apk@student.mahidol.edu>
2026-01-28 05:55:17 +0000

    test

--- main.cpp
+++ main.cpp
@@ -1,32 +1,45 @@
-//
-//  main.cpp
-//  First arg
-//
-//  Created by Mingmanas Sivaraksa on 2/1/2567 BE.
-//
-
 #include <iostream>
+#include <cstdlib>
+#include "sorting.h"
+
 using namespace std;
 
 int main(int argc, char * argv[]) {
-    
- 
-  cout<<"argc is "<<argc<<endl;
-    int i,N;
+
+    int i, N;
     int *a;
-    for(i=1;i<argc;i++){
-        cout<<"argv[" << i<<"]" <<argv[i]<<endl;
-      
+
+  
+    if (argc > 1) {
+        cout << "argc is " << argc << endl;
+        N = argc - 1;
+        a = new int[N];
+
+        for (i = 1; i < argc; i++) {
+            cout << "argv[" << i << "] " << argv[i] << endl;
+            a[i - 1] = atoi(argv[i]);
+        }
     }
-    /* Convert to numbers*/
-    N=argc-1;
-    cout<<"Before sorting:";
-    display(a,N);
     
-    sort(a,N);
-    cout<<"After sorting:";
-    display(a,N);
-    
-    
+    else {
+        cout << "How many numbers? ";
+        cin >> N;
+
+        a = new int[N];
+        cout << "Enter numbers: ";
+        for (i = 0; i < N; i++) {
+            cin >> a[i];
+        }
+    }
+
+    cout << "Before sorting: ";
+    display(a, N);
+
+    sort(a, N);
+
+    cout << "After sorting: ";
+    display(a, N);
+
+    delete[] a;
     return 0;
 }
--- sorting.h
+++ sorting.h
@@ -1,3 +1,28 @@
-void swap(int &a,int &b);
-void sort(int *a,int N);
-void display(int *a,int N);
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void sort(int *a, int N) {
+    int i, j;
+    for (i = 0; i < N - 1; i++) {
+        for (j = 0; j < N - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+}
+
+void display(int *a, int N) {
+    for (int i = 0; i < N; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+
