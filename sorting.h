3f5b2d133c7826df8a22bb9db9f88c0e29bd6241
KittayaApk <kittaya.apk@student.mahidol.edu>
2026-01-28 07:45:22 +0000

    finish

--- sorting.h
+++ sorting.h
@@ -1,28 +1,34 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+void swap(int &a, int&N);
+void sort(int &a, int&N);
+void display(int &a, int&N);
+
 
 void swap(int &a, int &b) {
     int temp = a;
     a = b;
     b = temp;
 }
-
+void display(int *a, int N) {
+    for (int i = 0; i < N; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
 void sort(int *a, int N) {
     int i, j;
     for (i = 0; i < N - 1; i++) {
         for (j = 0; j < N - i - 1; j++) {
             if (a[j] > a[j + 1]) {
                 swap(a[j], a[j + 1]);
+                cout <<" ";
+                display(a, N);
             }
         }
     }
-}
-
-void display(int *a, int N) {
-    for (int i = 0; i < N; i++) {
-        cout << a[i] << " ";
-    }
-    cout << endl;
 }
 
 
