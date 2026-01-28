d740341b1e5687c05a162f9c09dc8bd51098a4c2
KittayaApk <kittaya.apk@student.mahidol.edu>
2026-01-28 08:43:32 +0000

    finish

--- sorting.h
+++ sorting.h
@@ -2,36 +2,50 @@
 #include <iomanip>
 using namespace std;
 
-void swap(int &a, int&N);
-void sort(int &a, int&N);
-void display(int &a, int&N);
-
+void swap(int &a, int &b);
+void bubblesort(int *a, int N);
+void selectionsort(int *a, int N);
+void display(int *a, int N);
 
 void swap(int &a, int &b) {
     int temp = a;
     a = b;
     b = temp;
 }
+
 void display(int *a, int N) {
     for (int i = 0; i < N; i++) {
         cout << a[i] << " ";
     }
     cout << endl;
 }
-void sort(int *a, int N) {
+
+void bubblesort(int *a, int N) {
     int i, j;
     int sorted;
     for (i = 0; i < N - 1; i++) {
-        sorted =0 ;
+        sorted = 0;
         for (j = 0; j < N - i - 1; j++) {
             if (a[j] > a[j + 1]) {
                 swap(a[j], a[j + 1]);
-                cout <<" ";
                 sorted = 1;
                 display(a, N);
             }
         }
+        if (sorted == 0) break;
     }
+    
 }
 
-
+void selectionsort(int *a, int N) {
+    int i, j, mi;
+    for (i = 0; i < N - 1; i++) {
+        mi = i;
+        for (j = i + 1; j < N; j++) {
+            if (a[j] < a[mi]) {
+                mi = j;
+            }
+        }
+        swap(a[i], a[mi]);
+    }
+}
